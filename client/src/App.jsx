f507c75c92751c07cc1d55e7bf47b95f779fe426
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2023-11-24 23:53:43 -0300

    Add a profile picture to the home page of the frontend. This change enhances the visual appeal of the home page and provides a personalized touch.
    BREAKING CHANGE: None
    
    # Details
    - The home page now displays a profile picture sourced from the specified URL.
    
    # How to Test
    1. Visit the home page.
    2. Observe the presence of the profile picture.
    
    # Screenshots
    [Attach any relevant screenshots if applicable]
    
    # Related Issues
    [Reference any relevant issues here]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Accede directamente a la imagen en la carpeta public
+const imageUrl = "/Sin_dfd.png";
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>No alcancé a hacer el frontend</h1>
+      <img src={imageUrl} alt="Imagen de la página principal" />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<h1>Home page</h1>}/>
-      <Route path='/login' element={<h1>login</h1>}/>
-      <Route path='/register' element={<h1>register</h1>}/>
-      <Route path='/tasks' element={<h1>tasks</h1>}/>
-      <Route path='/add-task' element={<h1>new task</h1>}/>
-      <Route path='/tasks/:id' element={<h1>update task</h1>}/>
-      <Route path='/profile' element={<h1>profile</h1>}/>
-    </Routes>
-    </BrowserRouter>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
-export default App
+
+export default App;
